24b6a2a29d22bc5124b84d6d8eff1cde2d429068
abdelrahman <86198822+Abdelrahmann1@users.noreply.github.com>
2025-08-21 11:28:26 +0300

    adding sitemaps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Dewi
-* Template URL: https://bootstrapmade.com/dewi-free-multi-purpose-html-template/
-* Updated: Aug 07 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function(e) {
       const targetId = this.getAttribute('href');
